f86626d7479c71058dff67f52bfa1925d6846bed
Jake <Jake@172.31.5.41>
2014-04-08 11:27:54 -0400

    beginning of the project

--- src/com/example/asteroids/AsteroidView.java
+++ src/com/example/asteroids/AsteroidView.java
@@ -132,157 +132,33 @@ public class AsteroidView extends Activity {
 	float[] P = new float[3];
 	float[] V = new float[3];
 
-	public float mAngle;
-	private float[] mMMatrix = new float[16];
-	private float[] mMVPMatrix = new float[16];
-
-	private float[] mProjMatrix = new float[16];
-	private float[] mVMatrix = new float[16];
-	FloatBuffer mTriangleVB;
-
-
-	private int mProgram;
-	private int maPositionHandle;
-	private int muMVPMatrixHandle;
-	private final String vertexShaderCode =  
-			"uniform mat4 uMVPMatrix; \n" +
-					"attribute vec4 vPosition; \n" + 
-					"void main(){ \n" +
-					"   gl_Position = uMVPMatrix * vPosition; \n" +
-					"} \n";
-	private final String fragmentShaderCode =  
-			"precision mediump float; \n" + 
-					"void main(){ \n" + 
-					"   gl_FragColor = vec4 (0.63671875, 0.76953125, 0.22265625, 1.0); \n" + 
-					"} \n";
-
-
-
-
-
+	/** Called when the activity is first created. */
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_asteroid_view);
 
 		// Create GLSurfaceView
-		//  mGLView = new OpenGLSurfaceView(this); LOOK INTO THIS
+		mGLView = new OpenGLSurfaceView(this);
 		setContentView(mGLView);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.asteroid_view, menu);
-		return true;
-	}
-	public void onSurfaceCreated(GL10 unused, EGLConfig config) {       
-		// Set the background frame color
-		GLES20.glClearColor(0.1f,0.1f,0.1f,1.0f);
-		init();
-
-		// Load/Compile shaders from shader source
-		int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
-		int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
-
-		// Create shader program object
-		mProgram = GLES20.glCreateProgram();
-
-		// Attach vertex and fragment shader to program
-		GLES20.glAttachShader(mProgram, vertexShader);
-		GLES20.glAttachShader(mProgram, fragmentShader);
-
-		// Link shaders to create shader program
-		GLES20.glLinkProgram(mProgram);
-
-		// Get vertex shader variable reference
-		maPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
-		muMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+	protected void onPause() {
+		super.onPause();
+		// The following call pauses the rendering thread.
+		// If your OpenGL application is memory intensive,
+		// you should consider de-allocating objects that
+		// consume significant memory here.
+		mGLView.onPause();
 	}
 
-	private int loadShader(int type, String shaderCode){
-
-		// Create shader object of appropriate type
-		int shader = GLES20.glCreateShader(type); 
-
-		// Compile shader source
-		GLES20.glShaderSource(shader, shaderCode);
-		GLES20.glCompileShader(shader);
-
-		return shader;
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// The following call resumes a paused rendering thread.
+		// If you de-allocated graphic objects for onPause()
+		// this is a good place to re-allocate them.
+		mGLView.onResume();
 	}
-
-	public void init() {
-
-
-	}
-
-
-	public void onDrawFrame(GL10 unused) {    
-		// Clear frame and depth buffers
-		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
-
-		// Select shader program
-		GLES20.glUseProgram(mProgram);
-
-		// Attaches vertex buffer to shader
-		GLES20.glVertexAttribPointer(maPositionHandle, 3, GLES20.GL_FLOAT, false, 12, mTriangleVB);
-		GLES20.glEnableVertexAttribArray(maPositionHandle);
-
-		// Compute local transformation (rotation) matrix
-		Matrix.setRotateM(mMMatrix, 0, mAngle, 0, 0, 1.0f);
-
-		// Add local transformation into modelview matrix
-		Matrix.multiplyMM(mMVPMatrix, 0, mVMatrix, 0, mMMatrix, 0);
-
-		// Add modelview matrix to projection matrix
-		Matrix.multiplyMM(mMVPMatrix, 0, mProjMatrix, 0, mMVPMatrix, 0);
-
-		// Pass modelview-projection matrix to shader
-		GLES20.glUniformMatrix4fv(muMVPMatrixHandle, 1, false, mMVPMatrix, 0);
-
-		// Draw geometry
-		GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
-		GLES20.glDisableVertexAttribArray(maPositionHandle);    
-	}
-	private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
-	private float mPreviousX;
-	private float mPreviousY;
-
-	@Override 
-	public boolean onTouchEvent(MotionEvent event) {
-
-	   // Get locations from event
-	   float x = event.getX();
-	   float y = event.getY();
-
-	   switch (event.getAction()) {
-	      case MotionEvent.ACTION_MOVE:
-	         // Compute swipe distance
-	         float dx = x - mPreviousX;
-	         float dy = y - mPreviousY;
-
-	         // Change direction of rotation above the mid-line
-	         if (y > getHeight() / 2) {
-	            dx = dx * -1 ;
-	         }
-
-	         // Change direction of rotation to left of the mid-line
-	         if (x < getWidth() / 2) {
-	            dy = dy  * -1 ;
-	         }
-
-	         // Update rotation angle
-	         mRenderer.mAngle += (dx + dy) * TOUCH_SCALE_FACTOR;
-
-	         // Render updated frame
-	         requestRender();
-	   }
-
-	   // Store current locations
-	   mPreviousX = x;
-	   mPreviousY = y;
-
-	   return true;
-	}   
+	
 }
