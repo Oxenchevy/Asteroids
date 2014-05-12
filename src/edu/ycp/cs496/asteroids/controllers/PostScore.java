3425899b22db9cbd8d5c386e066ae416a8ba43da
Jake Oxenford <Jake Oxenford@Jakes-PC>
2014-05-11 22:13:46 -0400

    POST TO SERVER WORKS

---
+++ src/edu/ycp/cs496/asteroids/controllers/PostScore.java
@@ -0,0 +1,73 @@
+package edu.ycp.cs496.asteroids.controllers;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIUtils;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+import edu.ycp.cs496.asteroids.model.User;
+import edu.ycp.cs496.asteroids.model.json.JSON;
+
+/**
+ * Controller to get the complete inventory (list of items).
+ */
+public class PostScore {
+	 
+		public boolean postScore(String itemName, int score) throws URISyntaxException, JsonGenerationException, JsonMappingException, IOException { 
+
+			return makePostRequest(itemName,score); 
+		} 
+
+		public boolean makePostRequest(String itemName, int score) throws URISyntaxException, JsonGenerationException, JsonMappingException, IOException { 
+			// TODO: Implement method to issue delete inventory request 
+			// Create HTTP client 
+			HttpClient client = new DefaultHttpClient(); 
+
+			// Construct URI 
+			URI uri; 
+			uri = URIUtils.createURI("http", "23.21.105.22", 8081, "/leaderboard", 
+				    null, null);
+			// Construct request 
+			HttpPost request = new HttpPost(uri); 
+
+			// Execute request 
+
+			User newScore = new User(); 		
+
+			newScore.setName(itemName); 
+			newScore.setScore(score); 
+			StringWriter sw = new StringWriter(); 
+			JSON.getObjectMapper().writeValue(sw, newScore); 
+
+			// Add JSON object to request 
+			StringEntity reqEntity = new StringEntity(sw.toString()); 
+			reqEntity.setContentType("application/json"); 
+			request.setEntity(reqEntity); 
+
+			HttpResponse response = client.execute(request); 
+			// Parse response 
+			if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) { 
+				return true;                     
+			}  
+			else 
+			{ 
+				return false; 
+			} 
+		} 
+ 
+}
+
+
--- src/edu/ycp/cs496/main/GameOver.java
+++ src/edu/ycp/cs496/main/GameOver.java
@@ -1,6 +1,18 @@
 package edu.ycp.cs496.main;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.xml.sax.SAXException;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
 import edu.ycp.cs496.asteroids.controllers.AsteroidsSingleton;
+import edu.ycp.cs496.asteroids.controllers.PostScore;
 import edu.ycp.cs496.asteroids.model.Game;
 import android.os.Bundle;
 import android.app.Activity;
@@ -20,34 +32,51 @@ public class GameOver extends Activity {
 
 
 	private Game game; 
+	private String name;
+	private int score;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		 this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_game_over);
 
-		
-		// display game over
-		// display final score
-		// go to leader board or main menu
 		AsteroidsSingleton.getInstance();
 
 		game = AsteroidsSingleton.getGame();
 		TextView finalScore = (TextView)findViewById(R.id.finalScore); 
 		finalScore.setText(Integer.toString(game.getUser().getScore()));
 
+		final TextView UserName = (TextView)findViewById(R.id.txtName);
 
-		final TextView name = (TextView)findViewById(R.id.txtName);
 		Button submit = (Button) findViewById(R.id.Submitbtn);
-		submit.setOnClickListener(new View.OnClickListener(){
+		submit.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {				
-					// submit the information to the server and proceed to the leaderboard
-					Intent intent = new Intent(getBaseContext(), LeaderboardActivity.class);
-					startActivity(intent); 
+
+
+				try { 
+					name = UserName.getText().toString();
+					score = game.getUser().getScore();
+					if (name.length() > 0) 
+					{ 
+						// just add the name that was entered 
+						postItem(name, score); 
+						// if completed it should show a toast message that it was done 
+						Intent intent = new Intent(getBaseContext(), LeaderboardActivity.class);
+						startActivity(intent); 
+					} 
+					else 
+					{ 
+						// throw a toast error 
+						Toast.makeText(GameOver.this, "Fill in the name", Toast.LENGTH_SHORT).show(); 
+					} 
+				} 
+				catch (Exception e) { 
+					e.printStackTrace(); 
+				} 
 			}
-		});  
+		}) ;  
 	}
 
 	@Override
@@ -56,5 +85,16 @@ public class GameOver extends Activity {
 		getMenuInflater().inflate(R.menu.game_over, menu);
 		return true;
 	}
+
+	public void postItem(String name, int score) throws URISyntaxException, ClientProtocolException, 
+	IOException, ParserConfigurationException, SAXException{ 
+		PostScore ic = new PostScore(); 
+		boolean item = ic.postScore(name, score); 
+		if (item == true) { 
+			//Toast.makeText(GameOver.this, "Added " + score + " " + name, Toast.LENGTH_SHORT).show(); 
+		} else { 
+			//Toast.makeText(GameOver.this, "Could not add " + name, Toast.LENGTH_SHORT).show(); 
+		} 
+	} // end the postItem 
 
 }
